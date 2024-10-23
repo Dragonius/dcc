d743302d8f21bc2127883bd2d0e0136300c8f4ea
Dragonius <Blackerhawer@hotmail.com>
2024-10-23 18:16:53 +0300

    index.ts update

--- packages/apps/campaign/src/data/scenarios/index.ts
+++ packages/apps/campaign/src/data/scenarios/index.ts
@@ -2,14 +2,14 @@ import * as Types from "@kilcekru/dcc-shared-types";
 
 import * as eagleDown from "./eagle-down.json";
 import * as operationNorthernShield from "./operation-northern-shield.json";
+import * as operationsyriapump from "./operation-syria-pump.json";
 import * as redBullet from "./red-bullet.json";
 import * as roadToParis from "./road-to-paris.json";
-import * as operationsyriapump from "./operation-syria-pump.json";
 
 export const scenarioList: Array<Types.Campaign.Scenario> = [
 	Types.Campaign.Schema.scenario.parse(operationNorthernShield),
+	Types.Campaign.Schema.scenario.parse(operationsyriapump),
 	Types.Campaign.Schema.scenario.parse(redBullet),
 	Types.Campaign.Schema.scenario.parse(roadToParis),
 	Types.Campaign.Schema.scenario.parse(eagleDown),
-	Types.Campaign.Schema.scenario.parse(operationsyriapump),
 ];
